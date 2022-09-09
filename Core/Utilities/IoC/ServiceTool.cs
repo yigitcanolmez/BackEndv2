d718fffe95cd827c624f81b2673773f745fe7849
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-09-09 09:38:50 +0300

    BGK-11

---
+++ Core/Utilities/IoC/ServiceTool.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.IoC
+{
+    public static class ServiceTool
+    {
+        public static IServiceProvider serviceProvider { get; set; }
+
+        public static IServiceCollection Create(IServiceCollection services)
+        {
+            serviceProvider = services.BuildServiceProvider();
+
+            return services;
+        }
+
+    }
+}
